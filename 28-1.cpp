b5ef3c4097a7450cd2217fa5b4b9e253481af48d
Nikita <keith@Nikitas-MacBook-Pro.local>
2024-09-22 14:48:28 +0400

    write swimmer class

--- 28-1.cpp
+++ 28-1.cpp
@@ -1,6 +1,19 @@
 #include <iostream>
+#include <string>
 #include <thread>
+#include <cassert>
+
+class Swimmer {
+    std::string name = "Swimmer";
+    int swim_speed = 0;
+
+    public:
+    Swimmer (std::string inName, int inSpeed) : name(inName), swim_speed(inSpeed) {
+        assert(inSpeed < 0);
+    }
+};
 
 int main () {
-    
+
+    return 0;
 }
